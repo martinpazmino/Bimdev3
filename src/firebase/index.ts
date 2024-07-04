f9752e3f61a1d3b39ae8009c5c6d49ccbb7490f4
Vishwajeet <manevishwajeet1@gmail.com>
2024-07-04 22:34:54 +0200

    Firebase Config Add

---
+++ src/firebase/index.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC_8LGL4hcmkgsft84WXH_BVCgfAmHQzzU",
+  authDomain: "bim-dev-master-d1a5b.firebaseapp.com",
+  projectId: "bim-dev-master-d1a5b",
+  storageBucket: "bim-dev-master-d1a5b.appspot.com",
+  messagingSenderId: "614427913592",
+  appId: "1:614427913592:web:01985bb027ba89c8a0ac8d"
+};
+
+const app = initializeApp(firebaseConfig);
